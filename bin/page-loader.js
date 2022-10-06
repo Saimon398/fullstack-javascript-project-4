b445eda189346209b7486a2cc6d797eb1bf67259
alex_p <aleksborislavskij@gmail.com>
2022-10-06 13:48:55 +0300

    build(npm): install Nock and Axios

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -6,6 +6,15 @@ program
   .description('Page loader utility')
   .version('0.0.1')
   .option('-o --output [dir]', 'output dir (default: "/home/user/current-dir"')
-  .argument('<url>');
+  .argument('<url>')
+  .action((url) => {
+    /**
+     * Данная функция принимает на вход адрес страницы
+     * Скачивает ее из сети - то есть осуществляет GET-запрос по которому возвращается содержимое страницы
+     * Кладет ее в указанную директорию при флаге --output
+     * Возвращает полный путь к загруженному файлу
+     */
+    console.log('Everything is done!');
+  })
 
 program.parse(process.argv);
